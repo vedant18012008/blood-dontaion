f3713f0bd79f6a38cef1aeea2ffa4aa28c52202c
Vedant <146733103+vedant18012008@users.noreply.github.com>
2026-03-13 22:48:05 +0530

    Delete donations directory

--- donations/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class DonationsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'donations'
-
-    def ready(self):
-        import donations.signals  # noqa: F401
